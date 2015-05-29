57761f07e4706ae6a72750105fcbc7b41ff23536
mmmary314 <maryarocheleau@gmail.com>
2015-05-28 20:20:12 -0400

    Update UseMachine.java

--- com.gdi.vendingmachine/src/com/gdi/vendingmachine/UseMachine.java
+++ com.gdi.vendingmachine/src/com/gdi/vendingmachine/UseMachine.java
@@ -4,59 +4,82 @@ import java.util.Scanner;
 
 public class UseMachine {
 	
-	
 	public static void main(String[] args) {
 		System.out.println("Building a new vending machine");
 		
 		// initialize new machine object
+		Machine vendingMachine = new Machine();
 		
-		// make 5 items with prices and names
+		// make 10 items - 5 healthy, 5 not healthy
+		Item apple = new Item("apple", 1.00, true);
+		Item orange = new Item("orange", 1.25, true);
+		Item pear = new Item("pear", .75, true);
+		Item banana = new Item("banana", .50, true);
+		Item yogurt = new Item("yogurt", 2, true);
 		
 		// put all the items into the vending machine
+		vendingMachine.inventory[0] = apple;
+		vendingMachine.inventory[1] = orange;
+		vendingMachine.inventory[2] = pear;
+		vendingMachine.inventory[3] = banana;
+		vendingMachine.inventory[4] = orange;
 		
 		// now print what's available for the user to eat
-		
-		// while there are items in the vending machine take the following actions:
-		// print the contents of the vending machine
-		// give the user a list of options of what they can do
-		// take the action the user has specified 
-		
+		while(!checkIfEmpty(vendingMachine)) {
+			printContents(vendingMachine.inventory);
+			giveOptions(vendingMachine);
+			takeAction(vendingMachine);
+		}
 	}
 	
-	// method should iterate through the array and print out the items available
-	// for purchase in the vending machine
-	// be sure to account for the state when a space in the array doesn't have
-	// an item for purchase
 	public static void printContents(Item[] inventory) {
-		
+		for (int i = 0; i < inventory.length; i++) {
+			if(inventory[i] != null) {
+				System.out.println("     " + i + ": " + inventory[i].getName()+ " " + inventory[i].getPrice());
+			}
+			else {
+				System.out.println("     " + i + ": -");
+			}
+		}
+		System.out.println("");
 	}
 	
-	// give the user the following options:
-	// 0: put money into the machine
-	// 1: purchase an item from the machine
-	// 2: check how much money is in the machine
-	// 3: return change from the machine
-	// 4: turn off the machine
 	public static void giveOptions(Machine vendingMachine) {
-	
+		System.out.println("0: Put money into machine");
+		System.out.println("1: Purchase item");
+		System.out.println("2: Check how much money is in machine");
+		System.out.println("3: Return change");
+		System.out.println("4: Turn off machine");
 	}
 	
-	// take the action the user specified they want to do in the "giveOptions" method
-	// be sure to account for the state when a user enters an invalid number
 	public static void takeAction(Machine vendingMachine) {
 		Scanner scanner = new Scanner(System.in);
 		String input = scanner.nextLine();
 		
-		// the variable "input" is what the user typed into the console
-		// check this variable to see if it matches any of the input
-		// we outlined above 
-		
+		//check for valid input
+		if (input.equals("0")) {
+			// put money into machine
+			putMoneyIntoMachine(vendingMachine);
+		}
+		else if (input.equals("1")) {
+			purchase(vendingMachine);
+		}
+		else if (input.equals("2")) {
+			System.out.println(vendingMachine.getMoney());
+		}
+		else if(input.equals("3")) {
+			// return change
+			returnChange(vendingMachine);
+		}
+		else if (input.equals("4")) {
+			// turn off machine
+			turnOffMachine();
+		}
+		else {
+			System.out.println("Not a valid input");
+		}
 	}
 	
-	// this method purchases an item from the vending machine
-	// it should return TRUE if the item was purchased
-	// it should return FALSE if the item was not purchased (item does not exist
-	// or the vending machine didn't have enough money for the item)
 	public static boolean purchase(Machine vendingMachine) {
 		// current amount available to purchase things with
 		System.out.println("What number item would you like?");
@@ -65,15 +88,29 @@ public class UseMachine {
 		Item currentItem = vendingMachine.inventory[itemNumber];
 		
 		// check if item exists
+		if (vendingMachine.inventory[itemNumber] == null) {
+			System.out.println("No item there!");
+			return false;
+		}
+		
 		// check if enough money for purchase
-		// decrement money available 
-		// remove item from Inventory array
+		if (checkIfEnoughMoney(itemNumber, vendingMachine)){
+			System.out.println("Here's your " + currentItem.getName());
+			// decrement money available 
+			vendingMachine.setMoney(vendingMachine.getMoney() - currentItem.getPrice());
+			System.out.println("You have " + vendingMachine.money + " remaining");
+
+			// remove item from Inventory array
+			vendingMachine.inventory[itemNumber] = null;
+			return true;
+		}
+		else {
+			System.out.println("Not enough money for that item!");
+			return false;
+		}
+		
 	}
 	
-	// this method asks the user to input money
-	// it uses the machine's setter method to add the money the user input to the
-	// total amount of money that the machine has
-	// it prints out the amount of money the vending machine has available 
 	public static void putMoneyIntoMachine(Machine vendingMachine) {
 		System.out.println("Input money please");
 		Scanner scanner = new Scanner(System.in);
@@ -82,26 +119,37 @@ public class UseMachine {
 		System.out.println("Machine has " + vendingMachine.getMoney() + " available");
 	}
 	
-	// this method checks if there is enough money to purchase an item
-	// it returns true if there is enough money, and false if there is not
 	public static boolean checkIfEnoughMoney(int itemNumber, Machine vendingMachine) {
-		
+		double moneyAvailable = vendingMachine.getMoney();
+		Item[] inventory = vendingMachine.getInventory();
+		double itemPrice = inventory[itemNumber].getPrice();
+		if (moneyAvailable >= itemPrice) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 	
-	// this method returns the change left in the machine
-	// if there is no change available, it tells the user that
 	public static void returnChange(Machine vendingMachine) {
-		
+		if(vendingMachine.getMoney() > 0){
+			System.out.println("Returning $" + vendingMachine.getMoney());
+			vendingMachine.setMoney(0);
+		}
+		else {
+			System.out.println("No money in the machine");
+		}
 	}
 	
-	// this method checks if the vending machine has no more inventory. 
-	// it returns false if there is inventory left in the machine
-	// it returns true if the machine is empty
 	public static boolean checkIfEmpty(Machine vendingMachine) {
-		
+		for(int i = 0; i < vendingMachine.inventory.length; i++) {
+			if (vendingMachine.inventory[i] != null) {
+				return false;
+			}
+		}
+		return true;
 	}
 	
-	// this method shuts off the machine and ends the program
 	public static void turnOffMachine() {
 		System.out.println("Goodbye");
 		System.exit(0);
