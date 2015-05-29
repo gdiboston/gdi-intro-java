553e27035bd8ca0eb7b7448fcf12037c8a407827
mmmary314 <maryarocheleau@gmail.com>
2015-05-28 20:19:25 -0400

    Update Machine.java

--- com.gdi.vendingmachine/src/com/gdi/vendingmachine/Machine.java
+++ com.gdi.vendingmachine/src/com/gdi/vendingmachine/Machine.java
@@ -4,6 +4,23 @@ package com.gdi.vendingmachine;
 // create a constructor, getters and setters for each parameter
 
 public class Machine {
-
+	public double money;
+	public Item[] inventory;
+	
+	public Machine() {
+		money = 0;
+		inventory = new Item[5];
+	}
+	
+	public double getMoney() {
+		return this.money;
+	}
+	public Item[] getInventory() {
+		return this.inventory;
+	}
+	public double setMoney(double money) {
+		this.money = money;
+		return this.money;
+	}
 	
 }
