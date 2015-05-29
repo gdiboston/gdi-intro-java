0947e5cce380442608eb3e4d0ca95006ad648163
mmmary314 <maryarocheleau@gmail.com>
2015-05-28 20:20:40 -0400

    Update Item.java

--- com.gdi.vendingmachine/src/com/gdi/vendingmachine/Item.java
+++ com.gdi.vendingmachine/src/com/gdi/vendingmachine/Item.java
@@ -1,9 +1,27 @@
 package com.gdi.vendingmachine;
 
-
-// items should have a name and a price
-// create a constructor, getters and setters for this class
-
 public class Item {
-
+	String name;
+	public double price;
+	boolean healthy;
+	
+	public Item(String name, double price, boolean healthy) {
+		this.name = name;
+		this.price = price;
+		this.healthy = healthy;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	public double getPrice() {
+		return this.price;
+	}
+	public boolean getHealthy() {
+		return this.healthy;
+	}
+	public double setPrice(double price) {
+		this.price = price;
+		return this.price;
+	}
 }
